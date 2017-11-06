b4fbd9a3c4d919286e230145d504b9f87acef24f
Yosef Kaner <yknaer12@gmail.com>
2017-11-06 19:29:26 +0200

    the file

--- hw1/Q3.py
+++ hw1/Q3.py
@@ -6,23 +6,16 @@ import math
 
 
 def far_dist(point, points):
-    far = -float('inf')
-    for p in points:
-        far = max(far, Q1.dist(p, point))
     far_point = max(points, key=lambda p: Q1.dist(p, point))
-    if far != Q1.dist(far_point, point):
-        print(far, Q1.dist(far_point, point))
-    return far
+    return Q1.dist(far_point, point)
 
 
 errors = []
 points = np.random.rand(2000, 3) * 1000
 for e in np.arange(0.1, 1.1, 0.1):
-    print('epsilon: %f' % e)
     # a = raw_input()
     coreset_i = one_center_grid_coreset(points, e)
     coreset = points[coreset_i]
-    print(len(coreset))
 
     Q = np.random.rand(100, 3) * 10 * 1000
     error = -float('inf')
@@ -30,7 +23,6 @@ for e in np.arange(0.1, 1.1, 0.1):
         far_p = far_dist(q, points)
         far_c = far_dist(q, coreset)
         error = max(error, math.fabs(far_p - far_c) / far_p)
-    print(error)
     errors.append(error)
 
 
