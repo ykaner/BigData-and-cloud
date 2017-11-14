2a5f8787f9a7a5b1c119e7ff05783f443a52e498
Yosef Kaner <yknaer12@gmail.com>
2017-11-14 22:15:15 +0200

    submition hw1

--- hw1/Q1.py
+++ hw1/Q1.py
@@ -38,39 +38,3 @@ def one_center_grid_coreset(points, epsilon):
             coreset.append(i)
     return coreset
 
-
-if __name__ == '__main__':
-    points = np.array([
-        [1, 2, 3],
-        [2, 4, 6],
-        [2.01, 4.01, 5.99],
-        [5, 9, 34],
-        [5, 9, 34],
-        [35, 4, 43]
-    ])
-    # points = misc.face()
-    points = cv2.imread("C:\\Users\\ykane\\Pictures\\IMG_0709.JPG")
-    cv2.imshow('ps2', points)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    points = points.reshape(points.shape[0] * points.shape[1] / 2, points.shape[2] * 2)
-    points = points.astype(float)
-    print(points.shape)
-    coreset = one_center_grid_coreset(points, 0.1)
-    print('coreset: ' + str(coreset))
-    print('len(coreset): %d' % len(coreset))
-    # testing:
-
-    q = np.random.rand(6) * 250
-    print('q: ' + str(q))
-    m_c = 0
-    for c in coreset:
-        m_c = max(dist(points[c], q), m_c)
-    print('m_c: ' + str(m_c))
-    m_p = 0
-    for p in points:
-        m_p = max(dist(p, q), m_p)
-    print('m_p: ' + str(m_p))
-
-    print(m_c / m_p)
-    print(m_p / m_c)
